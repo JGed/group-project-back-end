fb9c77d04dfe184168fff9f3c5230e9dbd78f618
Josh Geddes <josh.geddes96@gmail.com>
2021-08-29 16:41:13 -0400

    pototype for user registration

---
+++ controllers/usercontroller.js
@@ -0,0 +1,67 @@
+const router = require('express').Router();
+const User = require('../db').import('../models/user');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const { Op } = require('sequelize');
+
+/*
+    User Registration
+*/
+router.post('/register', async (req, res) => {
+    const { email, username, password } = req.body.user;
+    try {
+        const existingUsers = await User.findAll({
+            where: {
+                [Op.or]: [{ email: email }, { username: username }],
+            },
+        });
+        // if the username or email is already registered construct a message to send back in the response
+        if (existingUsers.length > 0) {
+            let message = '';
+            for(existingUser of existingUsers) {
+                if(existingUser.email === email) {
+                    message += `${email} already registered with ClickNCook.  `
+                }
+                if(existingUser.username === username) {
+                    message += `${username} already registered with ClickNCook.  `
+                }
+            }
+            res.status(500).json({
+                message: message.trim()
+            });
+            return;
+        }
+    } catch (error) {
+        // catch error with findAll method and send it in the response
+        res.status(500).json({
+            message: 'problem searching for existing user',
+            error: error,
+        });
+        return;
+    }
+    try {
+        // create the new user in the table
+        const newUser = await User.create({
+            email: email,
+            username: username,
+            passwordhash: bcrypt.hashSync(password, 13),
+        });
+        // create a token with the newUser id
+        const token = jwt.sign({ id: newUser.id }, process.env.JWT_SECRET, {
+            expiresIn: 60 * 60 * 24,
+        });
+        // send a success message and token with the repsonse
+        res.status(200).json({
+            message: 'User sucessfully created!',
+            sesssionToken: token,
+        });
+    } catch (error) {
+        // catch error with create method and send it in the response
+        res.status(500).json({
+            messge: 'Something went wrong, please try again.',
+            error: error,
+        });
+    }
+});
+
+module.exports = router;
--- db.js
+++ db.js
@@ -1,1 +1,16 @@
-// database
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('ClickNCook', 'postgres', 'password', {
+    host: 'localhost',
+    dialect: 'postgres',
+});
+
+sequelize.authenticate()
+    .then(
+        () => console.log('Connection has been established sucessfully.')
+    )
+    .catch(
+        err => console.log('Unable to connect to the databse:', err)
+    );
+
+module.exports = sequelize;
