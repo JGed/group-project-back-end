d5e867a26b8cf705478c7f21d16e5ac6f65b0853
Josh Geddes <josh.geddes96@gmail.com>
2021-08-31 20:26:53 -0400

    test

--- controllers/recipecontroller.js
+++ controllers/recipecontroller.js
@@ -1,8 +1,8 @@
-let router = express.Router();
-const recipe = require("../db").import("./models/recipe");
+const router = require('express').Router();
+const Recipe = require('../db').import('./models/recipe');
 
 // create a new recipe
-router.post("/", (req,res) => {
+router.post('/', (req, res) => {
     const recipeEntry = {
         name: req.body.name,
         directions: req.body.directions,
@@ -12,48 +12,54 @@ router.post("/", (req,res) => {
         photoURL: req.body.photoURL,
     };
     Log.create(recipeEntry)
-    .then((recipe) => res.status(200).json(recipe))
-    .catch((err) => res.status(500).json({
-        error: err
-    }));
+        .then((recipe) => res.status(200).json(recipe))
+        .catch((err) =>
+            res.status(500).json({
+                error: err,
+            })
+        );
 });
 // get all recipes for a user
-router.get("/", validateSession, (req,res) => {
+router.get('/', validateSession, (req, res) => {
     let userid = req.user.id;
-    recipe.findAll({
+    Recipe.findAll({
         where: { user_id: userid },
     })
-    .then((recipes) => res.status(200).json (recipes))
-    .catch((err) => res.status(500).json({
-        error: err
-    }));
+        .then((recipes) => res.status(200).json(recipes))
+        .catch((err) =>
+            res.status(500).json({
+                error: err,
+            })
+        );
 });
 
 // get all publicly available recipes
-router.get("/", (req,res)
-=> {let userid = req.user.id;
-recipe.findAll({
-    where: { public: true },
+router.get('/', (req, res) => {
+    Recipe.findAll({
+        where: { public: true },
     })
-    .then((recipes) => res.status(200).json (recipes))
-    .catch((err) => res.status(500).json({
-        error: err
-    }));
+        .then((recipes) => res.status(200).json(recipes))
+        .catch((err) =>
+            res.status(500).json({
+                error: err,
+            })
+        );
 });
 // get recipe with id matching the request parameter
-router.get("/:entryId", function (req,res)
- {
-     let userid = req.user.id;
-recipe.findAll({
-    where: { id: entryId, user_id: userid },
+router.get('/:entryId', (req, res) => {
+    let userid = req.user.id;
+    Recipe.findAll({
+        where: { id: entryId, user_id: userid },
     })
-    .then((recipes) => res.status(200).json (recipes))
-    .catch((err) => res.status(500).json({
-        error: err
-    }));
+        .then((recipes) => res.status(200).json(recipes))
+        .catch((err) =>
+            res.status(500).json({
+                error: err,
+            })
+        );
 });
 // update the recipe matching the request parameter
-router.put("/update/:id", (req, res) => {
+router.put('/update/:id', (req, res) => {
     const updateRecipeEntry = {
         name: req.body.name,
         directions: req.body.directions,
@@ -62,19 +68,18 @@ router.put("/update/:id", (req, res) => {
         public: req.body.public,
         photoURL: req.body.photoURL,
     };
-    const query = { where: {id: req.params.entryId, user_id: req.user.id } };
-    Recipe.update(updateLogEntry, query)
-    .then((recipes) => res.status(200).json(recipes))
-    .catch((err) => res.status(500).json({ error: err }));
+    const query = { where: { id: req.params.entryId, user_id: req.user.id } };
+    Recipe.update(updateRecipeEntry, query)
+        .then((recipes) => res.status(200).json(recipes))
+        .catch((err) => res.status(500).json({ error: err }));
 });
 
-router.delete("/delete/:entryId",
-function (req, res) {
-    const query = {where: {id: req.params.entryId, user_id: req.user.id } };
+router.delete('/delete/:entryId', (req, res) => {
+    const query = { where: { id: req.params.entryId, user_id: req.user.id } };
 
     Recipe.destroy(query)
-        .then(() => res.status(200).json({ message: "Recipe Deleted" }))
+        .then(() => res.status(200).json({ message: 'Recipe Deleted' }))
         .catch((err) => res.status(500).json({ error: err }));
-    });
-    
-    module.exports = router;
+});
+
+module.exports = router;
