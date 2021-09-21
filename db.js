d626f27058f406bf14091e0d4a11ed5b9fe0d338
Josh Geddes <josh.geddes96@gmail.com>
2021-09-21 19:12:47 -0400

    fixed db file

--- db.js
+++ db.js
@@ -2,6 +2,12 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false, // very important
+          }
+      }
 });
 
 sequelize.authenticate()
